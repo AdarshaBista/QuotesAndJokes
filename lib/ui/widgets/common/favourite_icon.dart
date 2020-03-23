156184ac14399a469a19cd2923ddd2da81073f51
AdarshaBista <adrsh07@gmail.com>
2020-03-23 23:31:00 +0545

    Added bouncing animation to favourite icon

--- lib/ui/widgets/common/favourite_icon.dart
+++ lib/ui/widgets/common/favourite_icon.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class FavouriteIcon extends StatelessWidget {
+class FavouriteIcon extends StatefulWidget {
   final Color color;
   final bool isFavourite;
   final Function onPressed;
@@ -14,14 +14,56 @@ class FavouriteIcon extends StatelessWidget {
         assert(isFavourite != null);
 
   @override
+  _FavouriteIconState createState() => _FavouriteIconState();
+}
+
+class _FavouriteIconState extends State<FavouriteIcon>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0.0,
+      upperBound: 0.1,
+    )..addListener(() {
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  double _scale;
+
+  @override
   Widget build(BuildContext context) {
+    _scale = widget.isFavourite
+        ? 1.0 + _animationController.value * 3.0
+        : 1.0 - _animationController.value * 3.0;
     return GestureDetector(
-      onTap: onPressed,
-      child: Icon(
-        isFavourite ? Icons.favorite : Icons.favorite_border,
-        size: 24.0,
-        color: color,
+      onTap: () {
+        _animationController
+            .forward()
+            .then((_) => _animationController.reverse());
+        widget.onPressed();
+      },
+      child: Transform.scale(
+        scale: _scale,
+        child: favIcon,
       ),
     );
   }
+
+  Widget get favIcon => Icon(
+        widget.isFavourite ? Icons.favorite : Icons.favorite_border,
+        size: 24.0,
+        color: widget.color,
+      );
 }
